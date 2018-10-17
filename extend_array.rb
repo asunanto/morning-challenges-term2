b14c750537aab4b212f880e2c73ca38ee78b689a
asunanto <wibowo.sutanto91@gmail.com>
2018-10-18 08:58:55 +1000

    sync master

--- extend_array.rb
+++ extend_array.rb
@@ -1,7 +1,29 @@
 class Array
   def square
     # create your square method here
+    map{|x| x**2}
   end
-
-  # now fill in the rest
+  
+  def cube
+    map{|x| x**3}
+  end
+  
+  def average
+    sum/length
+  end
+  
+  def even
+    # select{|x| x if x.even?}
+    select(&:even?)
+  end
+  
+  def odd
+    # select{|x| x if x.odd?}
+    select(&:odd?)
+  end
+  
 end
+
+# now fill in the rest
+
+
